0b31e315f7d52ca82c80cafbda0f30bd8f8c23f7
Ungigdu <Ungigdu@protonmail.com>
2021-06-07 16:47:45 +0800

    add rinkeby erc721 contract

--- erc1155/app.js
+++ erc1155/app.js
@@ -1,50 +1,33 @@
 import {
-  erc20_abi
+  erc1155_owner_create_abi
 } from "./abi.js";
 
-export const balanceOf = async (token_address, user_address, provider) => {
-  let token = new ethers.Contract(token_address, erc20_abi, provider)
-  return (await token.balanceOf(user_address)).toString()
-}
-
-window.balanceOf = balanceOf
-
-export const tokenInfo = async (token_address, provider) => {
-  let token = new ethers.Contract(token_address, erc20_abi, provider)
+export const queryMetadata = async (token) => {
   let result = await Promise.all([
-    token.symbol(),
-    token.decimals()
+    token.name(),
+    token.symbol()
   ])
-  return [
-    result[0],
-    result[1].toString()
-  ]
+  return result
 }
 
-window.tokenInfo = tokenInfo
-
-const max =  "115792089237316195423570985008687907853269984665640564039457584007913129639935"
-
-export const approve = async (token_address, target_address, provider, signer) => {
-  let token = new ethers.Contract(token_address, erc20_abi, provider)
-  await token.connect(signer).approve(target_address, max)
+export const createNFT = async (token, tokenId, to, amount,signer) => {
+  await token.connect(signer).create(to, tokenId, amount, "", []);
 }
-
-window.approve = approve
 
 //bind----------------------------------------------
-$("#query_balance").click(async ()=>{
-  let token_address = $("#check_balance_token").val()
-  let account = $("#check_balance_account").val()
-  let balance = await balanceOf(token_address, account, window.provider)
-  let info = await tokenInfo(token_address, window.provider)
-  $("#amount").html(balance / 10**info[1])
-  $("#decimal").html(info[1])
-  $("#symbol").html(info[0])
+$("#query_metadata").click(async ()=>{
+  let token_address = $("#show_metadata_contract").val()
+  let token = new ethers.Contract(token_address, erc1155_owner_create_abi, window.provider)
+  let result = await queryMetadata(token)
+  $("#token_name").html(result[0])
+  $("#token_symbol").html(result[1])
 })
 
-$("#approve").click(async ()=>{
-  let token_address = $("#approve_token").val()
-  let target_address = $("#approve_account").val()
-  await approve(token_address, target_address, window.provider, window.me)
+$("#create_NFT").click(async ()=>{
+  let token_address = $("#create_nft_contract").val()
+  let token = new ethers.Contract(token_address, erc1155_owner_create_abi, window.provider)
+  let tokenId = $("#create_nft_id").val()
+  let create_to = $("#create_to").val()
+  let amount = $("#create_amount").val()
+  await createNFT(token, tokenId, create_to, amount, window.me)
 })
